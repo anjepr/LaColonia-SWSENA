27fb4e6a5f79c9681be429bd4c87460e527c6f38
fragata <fragata@INNOVACION09>
2018-01-30 11:25:54 -0500

    sigo en mi proyecto

---
+++ src/controlador/GestionEmpleado.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import ModeloDTO.Empleado;
+
+/**
+ *
+ * @author fragata
+ */
+public interface GestionEmpleadoInterface {
+    public Boolean Registrar (Empleado t );
+    public Boolean Actualizar (Empleado t);
+    public Boolean Eliminar (Integer identificacion);
+    public Empleado Buscar (Integer identificacion);
+}
