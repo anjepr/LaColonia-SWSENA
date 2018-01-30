8674bc8b8712f64428736b24e8395dd072d5ac82
fragata <fragata@INNOVACION09>
2018-01-30 07:48:44 -0500

    sigo subiendo

--- src/ConexionDB/connection.java
+++ src/ConexionDB/connection.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Conexion;
+package ConexionDB;
 
 /**
  *
  * @author fragata
  */
-public class Conexion {
+class connection {
     
 }
