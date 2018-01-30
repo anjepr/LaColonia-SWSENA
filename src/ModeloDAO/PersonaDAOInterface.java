4229bed91763476e76027cee8bd115dba99376de
fragata <fragata@INNOVACION09>
2018-01-30 11:16:52 -0500

    sigo en mi proyecto

---
+++ src/ModeloDAO/PersonaDAOInterface.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ModeloDAO;
+
+/**
+ *
+ * @author fragata
+ */
+public interface PersonaDAOInterface {
+    public Boolean Registrar (PersonaDAOImpl pd);
+    public Boolean Actualizar (PersonaDAOImpl pd);
+    public Boolean Eliminar (Integer identificacion);
+    public PersonaDAOImpl Buscar (Integer identificacion);
+}
+
