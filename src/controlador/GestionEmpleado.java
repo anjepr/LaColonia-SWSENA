43beeeaeaea3f105f9b08d7935cc6c893abdd453
fragata <fragata@INNOVACION09>
2018-01-30 11:31:29 -0500

    sigo en mi proyecto

--- src/controlador/GestionEmpleado.java
+++ src/controlador/GestionEmpleado.java
@@ -6,14 +6,36 @@
 package controlador;
 
 import ModeloDTO.Empleado;
+import java.util.ArrayList;
 
 /**
  *
  * @author fragata
  */
-public interface GestionEmpleadoInterface {
-    public Boolean Registrar (Empleado t );
-    public Boolean Actualizar (Empleado t);
-    public Boolean Eliminar (Integer identificacion);
-    public Empleado Buscar (Integer identificacion);
+public class GestionEmpleado {
+    private static ArrayList<Trabajador> datos;
+    
+    public GestionEmpleado(){
+        datos=new ArrayList<>();
+        Empleado e1 = new Empleado(1,"Anjelly","Katalina","Pelaez","Colmenares","famisanar",1);
+        Empleado e2 = new Empleado(2,"Edward","Adrian","Cabezas","Pelaez","famisanar",2);
+        datos.add(e1);
+        datos.add(e2);
+    }
+    
+    public ArrayList<Empleado> mostrarTodosLosUsusarios(){
+        return datos;
+    }
+    
+    public Boolean existe(Integer identificacion){
+        for(int i=0; i <datos.size(); i++){
+            if(datos.get(i).getNumident()==(identificacion)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    
 }
+
